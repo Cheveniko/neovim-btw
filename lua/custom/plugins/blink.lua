910a2abac04b49cc3a1afc22f6faf5adbccb81fb
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2025-04-06 23:25:00 -0500

    replace nvim-cmp for blink.cmp

---
+++ lua/custom/plugins/blink.lua
@@ -0,0 +1,57 @@
+return {
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = 'default' },
+
+    -- appearance = {
+    --   -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    --   -- Adjusts spacing to ensure icons are aligned
+    --   nerd_font_variant = 'mono',
+    -- },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    -- completion = { documentation = { auto_show = false } },
+    completion = { documentation = { auto_show = true } },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+
+      per_filetype = {
+        sql = { 'snippets', 'dadbod', 'buffer' },
+        mysql = { 'snippets', 'dadbod', 'buffer' },
+        plsql = { 'snippets', 'dadbod', 'buffer' },
+      },
+
+      providers = {
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
+        dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
+      },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
+
+    signature = { enabled = true },
+  },
+  opts_extend = { 'sources.default' },
+}
