5a4b5a09be4126c41467935072bebf018727de37
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2025-04-07 18:30:21 -0500

    snacks.nvim

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,40 @@
+-- for lsp rename of files in nvim tree
+local prev = { new_name = '', old_name = '' } -- Prevents duplicate events
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'NvimTreeSetup',
+  callback = function()
+    local events = require('nvim-tree.api').events
+    events.subscribe(events.Event.NodeRenamed, function(data)
+      if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
+        data = data
+        Snacks.rename.on_rename_file(data.old_name, data.new_name)
+      end
+    end)
+  end,
+})
+
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+
+    -- image = { enabled = true },
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
+    -- dashboard = { enabled = true },
+    -- explorer = { enabled = true },
+    -- indent = { enabled = true },
+    -- input = { enabled = true },
+    -- picker = { enabled = true },
+    -- notifier = { enabled = true },
+    -- scope = { enabled = true },
+    -- scroll = { enabled = true },
+    -- statuscolumn = { enabled = true },
+    -- words = { enabled = true },
+  },
+}
