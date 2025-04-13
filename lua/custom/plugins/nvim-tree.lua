11ac6e6b4d2d9e7bdd893383055682a7e2a65037
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2025-04-13 02:32:34 -0500

    fix open tabs 🙏🏻

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -9,8 +9,18 @@ local function my_on_attach(bufnr)
   vim.keymap.set('n', '<C-f>', api.tree.toggle)
   vim.keymap.set('n', 'J', api.node.navigate.sibling.next, opts 'Next Sibling')
   vim.keymap.set('n', 'K', api.node.navigate.sibling.prev, opts 'Previous Sibling')
-  vim.keymap.del('n', '>', { buffer = bufnr })
-  vim.keymap.del('n', '<', { buffer = bufnr })
+  vim.keymap.del('n', '>', opts 'Increase Width')
+  vim.keymap.del('n', '<', opts 'Decrease Width')
+
+  local swap_then_open_tab = function()
+    local node = api.tree.get_node_under_cursor()
+    vim.cmd 'wincmd l'
+    api.node.open.tab(node)
+    vim.cmd 'NvimTreeOpen'
+    vim.cmd 'wincmd l'
+  end
+
+  vim.keymap.set('n', '<C-t>', swap_then_open_tab, opts 'Open in new tab')
 end
 
 return {
