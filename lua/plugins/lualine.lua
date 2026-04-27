887b3f97a0761c3d54abbe37d8559dabb0e17202
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2026-04-26 21:26:12 -0500

    not needed

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,6 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      theme = 'catppuccin-nvim',
       component_separators = '',
       section_separators = { left = '', right = '' },
     },
