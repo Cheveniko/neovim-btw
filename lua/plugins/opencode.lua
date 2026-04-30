fcb382f01767c2bce484233eadc046278edb0dfc
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2026-04-30 10:27:02 -0500

    new keymaps to be the same as opencode

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,18 +1,25 @@
 return {
   'sudo-tee/opencode.nvim',
   config = function()
+    local function insert_newline()
+      local enter = vim.api.nvim_replace_termcodes('<cr>', true, false, true)
+      vim.api.nvim_feedkeys(enter, 'n', false)
+    end
+
     require('opencode').setup {
       keymap = {
         editor = {
-          ['<leader>oc'] = { 'quick_chat', mode = { 'n', 'x' } },
+          ['<leader>oc'] = { 'quick_chat', mode = { 'n', 'x' }, desc = 'Quick chat' },
         },
         input_window = {
-          ['<esc>'] = false,
-          ['<tab>'] = { 'switch_mode' },
+          ['<esc>'] = { 'cancel', defer_to_completion = true, desc = 'Cancel running request' },
+          ['<tab>'] = { 'switch_mode', desc = 'Switch agent mode' },
+          ['<cr>'] = { 'submit_input_prompt', mode = { 'n', 'i' }, desc = 'Submit prompt' },
+          ['<S-cr>'] = { insert_newline, mode = { 'i' }, desc = 'Insert newline' },
         },
         output_window = {
-          ['<esc>'] = false,
-          ['<tab>'] = { 'switch_mode' },
+          ['<esc>'] = { 'cancel', defer_to_completion = true, desc = 'Cancel running request' },
+          ['<tab>'] = { 'switch_mode', desc = 'Switch agent mode' },
         },
       },
     }
